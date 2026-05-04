0b1d313100a31a5bdddf87a4da1baf927caf7033
anya <nyuta1909@gmail.com>
2026-05-04 09:34:43 +0300

    config.php

---
+++ api/public/config.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+
+$host = '127.0.0.1';
+$port = '5432';
+$dbname = 'php_site_19';
+$user = 'postgres';
+$password = '222';
+
+try {
+    $pdo = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname",
+        $user,
+        $password,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+} catch (PDOException $e) {
+    die(json_encode(['error' => 'Database connection failed: ' . $e->getMessage()]));
+}
+?>
